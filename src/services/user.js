9c4928e1e17ef0d8241c9711cd8d67534ba8c43a
Wesley <huchen@thoughtworks.com>
2018-06-12 20:50:04 +0800

    user services init

--- src/services/user.js
+++ src/services/user.js
@@ -1,9 +1,49 @@
 import { Ajax } from './ajax'
 
+/**
+ *
+ * @param {*} params
+ *
+ * {
+  "email": "string",
+  "password": "string"
+}
+ *
+ * @returns
+ * {
+  "data": {
+    "email": "string",
+    "id": 0,
+    "password": "string"
+  },
+  "message": "string",
+  "status": "string"
+}
+ */
 export function Register(params) {
   return Ajax('/api/register', params)
 }
 
+/**
+ *
+ * @param {*} params
+ *
+ * {
+  "email": "string",
+  "password": "string"
+}
+ *
+ * @returns
+ * {
+  "data": {
+    "email": "string",
+    "id": 0,
+    "password": "string"
+  },
+  "message": "string",
+  "status": "string"
+}
+ */
 export function Login(params) {
   return Ajax('/api/login', params)
 }
