a1514044a6d24ec71af82e14841aeb7aff05c7e7
Wesley <huchen@thoughtworks.com>
2018-06-12 20:20:27 +0800

    add proxy support --env.p

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # nho-pro-fe
+
 nho learn front end project
+
+## /api proxy
+
+cmd example: `yarn dev --env.p 192.169.3.2`
--- config/index.js
+++ config/index.js
@@ -3,14 +3,22 @@
 // see http://vuejs-templates.github.io/webpack for documentation.
 
 const path = require('path')
+const args = require('yargs').argv
+
+const { env = {} } = args
+
+const apiProxyHost = env.p || 'localhost:8080'
+
+console.log('api proxy host address:',  apiProxyHost, env)
 
 module.exports = {
   dev: {
-
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/api': `http://${apiProxyHost}`
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
