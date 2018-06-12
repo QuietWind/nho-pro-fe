2838338533cee6ccf67df7bb9371f3f3bd8b0342
Matthew5 <jnwu@CNjnwu.local>
2018-06-12 19:44:08 +0800

    [Matthew5] Add student camps page

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <router-link :to="{name: 'StudentCamps'}">Check your camps</router-link>
     <router-view/>
   </div>
 </template>
