dabea6be7445b1aec8e01d921ccb2f37874be387
huhongxiang <gohuhx@163.com>
2018-06-13 11:19:35 +0800

    注销页面修改

--- src/components/Logout.vue
+++ src/components/Logout.vue
@@ -1,9 +1,12 @@
 <template>
   <div>
     <div class="center">
-      <div>
-        <button class="button" @click="logout()">退出登录</button>
+      <div class="logout-info">
+        {{logoutStatus}}
       </div>
+      <div class="input-hanlde-line">
+            <button class="button" @click="backlogin">返回登录</button>
+          </div>
     </div>
   </div>
 </template>
@@ -12,167 +15,40 @@
 import { Logout } from './../services/user'
 
 export default {
-  name: 'login',
+  name: 'logout',
   data() {
     return {
-      msg: '',
-      email: '',
-      password: ''
+      logoutStatus: ''
     }
   },
-  methods: {
-    logout() {
-      Logout({
-       
-      }).then(res => {
-        console.log(res)
-        if(res.data.status == "OK") {
-          alert("成功退出")
+  mounted() {
+    Logout().then(res => {
+        if(res && res.data && res.data.status == "OK") {
+          this.logoutStatus = "成功退出";
         } else {
-          alert(res.data.message)
+          this.logoutStatus = res.data.message;
         }
       })
+  },
+  methods: {
+    backlogin: function() {
+      this.$router.push('/')
     }
   }
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang='postcss' scoped>
-@font-face {
-  font-family: 'iconfont';
-  src: url('/static/fonts/iconfont.eot');
-
-  /* IE9*/
-  src: url('/static/fonts/iconfont.eot?#iefix') format('embedded-opentype'),
-    url('/static/fonts/iconfont.woff') format('woff'),
-    url('/static/fonts/iconfont.ttf') format('truetype'),
-    url('/static/fonts/iconfont.svg#iconfont') format('svg');
-  /* iOS 4.1- */
-}
-
-.iconfont {
-  font-family: 'iconfont' !important;
-  font-size: 16px;
-  font-style: normal;
-  -webkit-font-smoothing: antialiased;
-  -webkit-text-stroke-width: 0.2px;
-  -moz-osx-font-smoothing: grayscale;
-}
-
-.icon-lock:before {
-  content: '\f0045';
-}
-
-.icon-user:before {
-  content: '\e605';
-}
-
-.icon-qrcode:before {
-  content: '\e855';
-}
-
-.icon-iconpc:before {
-  content: '\e647';
-}
-
-.icon-icscan24px:before {
-  content: '\e656';
-}
-
-.login-wrapper {
-  position: relative;
-  overflow: hidden;
-  max-width: 350px;
-  min-height: 300px;
-  margin: 0 auto;
-  background-color: #fff;
-  padding: 25px;
-  box-sizing: border-box;
-}
-h3 {
-  font-size: 15px;
-  font-weight: 600;
-}
-
-.iconfont {
-  position: absolute;
-  width: 52px;
-  height: 52px;
-  color: #4bb6ed;
-  font-size: 40px;
-  text-align: center;
-  line-height: 52px;
-  cursor: pointer;
-}
-
-.input-line {
-  position: relative;
-  margin: 20px 0 0;
-  z-index: 2;
-
-  .iconfont {
-    color: #fff;
-  }
-}
-
-.other-handle {
-  position: relative;
-  line-height: 18px;
-  margin: 8px 0;
-}
-
-a {
-  color: #666;
-  font-size: 12px;
-  text-decoration: none;
-
-  &:hover {
-    color: #4bb6ed;
-    text-decoration: underline;
-  }
-}
-
-.quick-register {
-  position: absolute;
-  right: 0;
-  top: 0;
-}
-
-.input-line--label {
-  position: absolute;
-  left: 1px;
-  top: 1px;
-  height: 40px;
-  width: 40px;
-  line-height: 40px;
-  text-align: center;
-  font-size: 20px;
-  background-color: #eee;
-}
-
-.input-line--input {
-  border: 1px solid #ddd;
-  padding: 11px 8px 11px 50px;
-  background-color: #fff;
-  width: 100%;
-  height: 42px;
-  box-sizing: border-box;
-
-  &:focus {
-    border-color: #4bb6ed;
-    box-shadow: none;
-    outline: 0;
-  }
-
-  &.warn {
-    background-color: #f34;
-  }
+.logout-info {
+  margin-top: 20px;
+  text-align: center
 }
 
 .button {
   display: block;
-  width: 100%;
+  width: 20%;
   height: 42px;
   line-height: 42px;
   text-align: center;
@@ -185,17 +61,10 @@ a {
   transition: all 350ms ease-in;
   color: #fff;
   border-radius: 2px;
-  cursor: pointer;
-
-  &:hover {
-    background-color: lighten(#4bb6ed, 5%);
-  }
+  cursor: pointer
 }
 
-.error-info {
-  color: red;
-  font-size: 12px;
-  min-height: 20px;
-  line-height: 20px;
+.input-hanlde-line {
+  width: 100%;
 }
 </style>
